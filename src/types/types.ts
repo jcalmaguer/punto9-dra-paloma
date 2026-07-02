0158a45c13d50eba84fe4bb4344e9351a3fdf37e
Carlos Almaguer <carlos.almaguer@entornolab.com>
2026-07-02 17:58:22 -0600

    feat(types): add PerfilData and sub-interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,4 @@
 export type { NavLink, NavbarData } from './navbar'
 export type { HeroTitle, HeroLink, HeroCta, HeroPhoto, HeroStat, HeroData } from './hero'
 export type { SpecialtyItem, SpecialtiesData } from './specialties'
+export type { ProfileBadge, ProfileCredential, ProfileTitle, ProfileCta, ProfileData } from './profile'
